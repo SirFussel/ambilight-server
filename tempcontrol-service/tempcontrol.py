ee21af596b134f386f3ca7cc83f78e07629df756
SirFussel <dieterichjan@yahoo.de>
2022-10-01 18:47:02 +0200

    Added source of code fragment

--- tempcontrol-service/tempcontrol.py
+++ tempcontrol-service/tempcontrol.py
@@ -10,6 +10,9 @@ import signal
 
 
 class GracefulKiller:
+    """
+    https://stackoverflow.com/questions/18499497/how-to-process-sigterm-signal-gracefully/31464349#31464349
+    """
     def __init__(self):
         self.kill_now = False
         signal.signal(signal.SIGINT, self.exit_gracefully)
